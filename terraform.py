ffcfc00fd7e9699ad5b4f4e9fd3fb9d8117198e4
Elizaveta Bodean <lizabodean@gmail.com>
2023-09-15 21:26:05 +0300

    added .env and .gitignore

--- terraform.py
+++ terraform.py
@@ -1,22 +1,25 @@
 import requests
-import json
+import random
 
-# Define your Terraform Cloud API token and organization name
-api_token = 'MpFhzJUy7JMbHw.atlasv1.c6zqvTdzgCFxziuyYtNh6u8xKzeqmTEDP5xO8YBYu7juz83sM54THVznv6Qdk7OH528'
-organization = 'Practice-UTM'
+import os
+from dotenv import load_dotenv
 
-# Set the base URL for Terraform Cloud API
-base_url = f'https://app.terraform.io/api/v2/organizations/Practice-UTM'
+load_dotenv()
+#environment variables
+
+api_token = os.getenv('API_TOKEN')
+organization = os.getenv('ORGANIZATION')
+base_url = os.getenv('BASE_URL')
 
 # Define headers with the API token
 headers = {
-    "Authorization": f'Bearer {api_token}',
-    "Content-Type": 'application/vnd.api+json',
+    'Authorization': f'Bearer {api_token}',
+    'Content-Type': 'application/vnd.api+json',
 }
 
 # Example: List workspaces
 def list_workspaces():
-    url = f"{base_url}/workspaces"
+    url = f'{base_url}/workspaces'
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
@@ -24,26 +27,29 @@ def list_workspaces():
         for workspace in workspaces['data']:
             print(workspace['attributes']['name'])
     else:
-        print(f"Failed to retrieve workspaces. Status code: {response.status_code}")
+        print(f'Failed to retrieve workspaces. Status code: {response.status_code}')
 
 # Example: Create a workspace
 def create_workspace(workspace_name):
-    url = f"{base_url}/workspaces"
+    url = f'{base_url}/workspaces'
     data = {
-        "data": {
-            "type": "workspaces",
-            "attributes": {
-                "name": workspace_name
+        'data': {
+            'type': 'workspaces',
+            'attributes': {
+                'name': workspace_name
             }
         }
     }
     response = requests.post(url, headers=headers, json=data)
 
     if response.status_code == 201:
-        print(f"Workspace '{workspace_name}' created successfully.")
+        print(f'Workspace "{workspace_name}" created successfully.')
     else:
-        print(f"Failed to create workspace. Status code: {response.status_code}")
+        if response.status_code == 422:
+            print(f'Failed to create workspace. workspace was created')  
+        else:
+            print(f'Failed to create workspace. Status code: {response.status_code}')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     list_workspaces()
-    create_workspace("my_new_workspace3")  # Uncomment to create a new workspace
+    create_workspace(f'workspace_{random.randint(1, 200)}')
